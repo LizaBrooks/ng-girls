c51d85d6614c869c2f2badb63c72d3972277ffe1
LizaBrooks <olena.pedash@gmail.com>
2020-11-24 18:36:07 +0200

    Your Message

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -6,29 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-button-unit.component.css']
 })
 export class InputButtonUnitComponent implements OnInit {
-  title = 'Hello word';
+  title = 'Hello World';
 
-  constructor() {
-    // this.changeTitle('My First Angular App');
-    // console.warn(this.title);
+  constructor() { }
+
+  ngOnInit() {
   }
 
-
-  ngOnInit(): void {
-    // this.title = 'Angular CLI Rules!';
-    setTimeout(() => {
-      this.title = 'This is not the title you are looking for';
-    }, 3000);
+  changeTitle(inputElementReference) {
+    console.dir(inputElementReference);
+    this.title = inputElementReference.value;
   }
-
-  changeTitle(newTitle: string) {
-    this.title = newTitle;
-  }
-
-
-
-  // changeTitle(event): void {
-  //   console.log(event);
-  //   this.title = event.target.value; // the original functionality still works
-  // }
 }
